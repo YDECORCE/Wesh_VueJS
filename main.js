37a38b216d647fb500f71cb7df53881bd1a856fc
YDECORCE <y.decorce@codeur.online>
2021-01-28 17:02:41 +0100

    mise en place axios

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue !'
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false
     }
-  })
+  },
+  created() {
+    axios
+      .get('https://fakestoreapi.com/products')
+      .then(response => {
+        this.info = response.data
+
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+})

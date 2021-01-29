b4e659b6ab413ef92db5b963f49e277a2e0d7bd1
YDECORCE <y.decorce@codeur.online>
2021-01-29 14:52:21 +0100

    before branche dev

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
+let productcard = {
+  data (){
+    return{
+    active: false
+    }
+  },
+  props: ['message'],
+  template: `<div class="card" @mouseover="mouseOver">
+              <div class="image">
+                <img v-bind:src = message.image alt="image" style="max-width:80%; height:auto">
+              </div>
+              <div class="title">{{message.title}}</div>
+              <div class="price">{{message.price + " €"}}</div>
+            </div>
+            <div v-show="active" class="cardover">
+              <button> add to cart</button>
+            </div>`,
+  methods:{
+    mouseOver: function() {
+        this.active= !this.active;
+    }
+      },
+  
+}
+
+
 new Vue({
   el: '#app',
+  components:{productcard},
   data() {
     return {
       info: null,
